48f968d662a743cd7b6cac8ee7628c07e39dd277
Osmany Pujol <Osmany.Pujolcs@gmail.com>
2021-09-02 00:02:47 -0400

    created and successfully retrieved data using the api request

--- app/src/main/java/com/osmany/flix/MainActivity.java
+++ app/src/main/java/com/osmany/flix/MainActivity.java
@@ -2,13 +2,48 @@ package com.osmany.flix;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.os.Bundle;
+import android.util.Log;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import okhttp3.Headers;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    public static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AsyncHttpClient client = new AsyncHttpClient();//   1. first step is to create a AsynchHttpclient object
+        client.get(NOW_PLAYING_URL, new JsonHttpResponseHandler() { //2. Make the request to the server and
+            // handle the on success and on failure reponses
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.d(TAG, "onSuccess"); // this is logging this message
+                JSONObject jsonObject = json.jsonObject;
+                try {
+                    JSONArray results = jsonObject.getJSONArray("results");
+                    Log.i(TAG,"Results: "+results.toString());
+                } catch(JSONException e){
+                    Log.e(TAG, "Hit json exception",e);
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.d(TAG, "onFailure"); // this is logging this message
+            }
+        });
     }
 }
